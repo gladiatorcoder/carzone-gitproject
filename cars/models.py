1b005baf374418355cec985493e949de2d061004
gladiatorcoder <fightfreeeak@gmail.com>
2022-09-15 22:53:21 +0530

    Added requirements.txt

--- cars/models.py
+++ cars/models.py
@@ -95,7 +95,7 @@ class Car(models.Model):
     year = models.IntegerField(('year'), choices=year_choice)
     condition = models.CharField(max_length=100)
     price = models.IntegerField()
-    description = RichTextField()
+    description = RichTextField(max_length=5000)
     car_photo = models.ImageField(upload_to='photos/%Y/%m/%d')
     car_photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
     car_photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
@@ -115,4 +115,8 @@ class Car(models.Model):
     no_of_owners = models.CharField(max_length=100)
     is_featured = models.BooleanField(default=False)
     created_date = models.DateTimeField(default=datetime.now, blank=True)
+
+
+    def __str__(self):
+    	return self.car_title
 
